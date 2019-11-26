123649e42170e65364b6e2136b6d4ea95195fe3b
brandinooo98 <bterickson2@wisc.edu>
2019-11-25 23:01:43 -0600

    Finished

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,24 @@
 package application;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
 
 /**
  * JDK 11, Eclipse 2019-06
@@ -25,7 +34,7 @@ public class Main extends Application {
 
     private static final int WINDOW_WIDTH = 300;
     private static final int WINDOW_HEIGHT = 200;
-    private static final String APP_TITLE = "Hello World!";
+    private static final String APP_TITLE = "CS400 MyFirstJavaFX";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -37,13 +46,31 @@ public class Main extends Application {
         for (String arg : args) {
             vbox.getChildren().add(new Label("hello "+arg));
         }
+        // Combo box
+        String colors[] = {"Red", "Yellow", "Blue"};
+        ComboBox comboBox = new ComboBox(FXCollections.observableArrayList(colors));
+
+        // Image view
+        FileInputStream file = new FileInputStream("brandon.png");
+        Image image = new Image(file);
+        ImageView imageView = new ImageView(image);
+
+        // Button
+        Button button = new Button("Done");
+
+        // Hyper Link
+        Hyperlink hyperLink = new Hyperlink("Click");
+        VBox vBox = new VBox(hyperLink);
 
         // Main layout is Border Pane example (top,left,center,right,bottom)
         BorderPane root = new BorderPane();
 
         // Add the vertical box to the center of the root pane
         root.setTop(new Label(APP_TITLE));
-        root.setCenter(vbox);
+        root.setCenter(imageView);
+        root.setLeft(comboBox);
+        root.setBottom(button);
+        root.setRight(vBox);
         Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 
         // Add the stuff and set the primary stage
